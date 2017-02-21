4f015f3af8b6920a316507deb5fb5ec6be4553c7
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-02-21 11:38:22 +0100

    Doc Preconditions

--- src/ch/epfl/alpano/Preconditions.java
+++ src/ch/epfl/alpano/Preconditions.java
@@ -1,7 +1,7 @@
 package ch.epfl.alpano;
 
 /**
-*
+* Interface fournissant des méthodes de contrôle
 *
 * @author Philippine Favre (258854)
 * @author Bastien Beuchat  (257117)
@@ -9,13 +9,24 @@ package ch.epfl.alpano;
 
 public interface Preconditions {
 	
+	/**
+	 * Lève une exception si son argument est faux
+	 * @param b expression booléenne à vérifier
+	 * @throws IllegalArgumentException exception levée si le paramêtre booléen est faux
+	 */
 	public static void checkArgument(boolean b) throws IllegalArgumentException{
 		if(!b){
 			throw new IllegalArgumentException();
 		}
 	}
 	
-	public static void checkArgument(boolean b, String message){
+	/**
+	 * Lève une exception si son argument est faux
+	 * @param b expression booléenne à vérifier
+	 * @param message message d'erreur
+	 * @throws IllegalArgumentException exception levée si le paramêtre booléen est faux
+	 */
+	public static void checkArgument(boolean b, String message) throws IllegalArgumentException{
 		if(!b){
 			throw new IllegalArgumentException(message);
 		}
