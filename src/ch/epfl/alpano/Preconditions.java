26e1915b9b598b597d302cf1e8ae9790688e4304
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-02-21 11:26:34 +0100

    Preconditions implémenté (unit test passent)

--- src/ch/epfl/alpano/Preconditions.java
+++ src/ch/epfl/alpano/Preconditions.java
@@ -1,5 +1,23 @@
 package ch.epfl.alpano;
 
-public interface Preconditions {
+/**
+*
+*
+* @author Philippine Favre (258854)
+* @author Bastien Beuchat  (257117)
+*/
 
+public interface Preconditions {
+	
+	public static void checkArgument(boolean b) throws IllegalArgumentException{
+		if(!b){
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	public static void checkArgument(boolean b, String message){
+		if(!b){
+			throw new IllegalArgumentException(message);
+		}
+	}
 }
