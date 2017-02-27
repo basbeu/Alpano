56075e6603539ff9dfe23710e76b57e7dda75a1b
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-02-27 22:19:42 +0100

    Création des fichiers et de leur structure : partie 3

--- src/ch/epfl/alpano/dem/CompositeDiscreteElevationModel.java
+++ src/ch/epfl/alpano/dem/CompositeDiscreteElevationModel.java
@@ -1,11 +1,34 @@
 package ch.epfl.alpano.dem;
 
+import ch.epfl.alpano.Interval2D;
+
 /**
  * 
  *
  * @author Philippine Favre (258854)
  * @author Bastien Beuchat  (257117)
  */
-public class CompositeDiscreteElevationModel {
+final class CompositeDiscreteElevationModel implements DiscreteElevationModel {
+    public CompositeDiscreteElevationModel(DiscreteElevationModel dem1, DiscreteElevationModel dem2) {
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void close() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
 
+    @Override
+    public Interval2D extent() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double elevationSample(int x, int y) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
 }
