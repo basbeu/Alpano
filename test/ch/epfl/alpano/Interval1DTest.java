c94d5d74ee23f4956df316f40f14b9b657e707ae
phili <phili@192.168.0.150>
2017-02-27 09:17:43 +0100

    TestJunit Interval1D

---
+++ test/ch/epfl/alpano/Interval1DTest.java
@@ -0,0 +1,129 @@
+package ch.epfl.alpano;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Interval1DTest {
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testConstructeur() {
+        Interval1D interval = new Interval1D(6,0);
+    }
+    
+    @Test
+    public void testConstructeurOk(){
+        Interval1D it = new Interval1D (0,6);
+    }
+    
+    @Test 
+    public void testIncludedFrom(){
+        Interval1D interval = new Interval1D(0,6);
+        assertEquals(0, interval.includedFrom(), 0);
+    }
+    
+    @Test
+    public void testIncludedTo() {
+        Interval1D interval = new Interval1D(0,6);
+        assertEquals(6, interval.includedTo(), 0);
+    }
+    
+    @Test
+    public void testContains(){
+        Interval1D interval = new Interval1D(0,6);
+        assertTrue(interval.contains(5));
+    }
+    
+    @Test
+    public void testContainsUp(){
+        Interval1D interval = new Interval1D(0,6);
+        assertTrue(interval.contains(6));
+    }
+    
+    @Test
+    public void testContainsDown(){
+        Interval1D interval = new Interval1D(0,6);
+        assertTrue(interval.contains(0));
+    }
+    
+    @Test
+    public void testContainsWrong(){
+        Interval1D interval = new Interval1D(0,6);
+        assertTrue(interval.contains(8));
+    }
+    
+    @Test
+    public void testSize(){
+        Interval1D interval = new Interval1D(0,3);
+        assertEquals(4, interval.size(), 0);
+    }
+    
+    @Test
+    public void testSizeZero(){
+        Interval1D interval = new Interval1D(0,0);
+        assertEquals(1, interval.size(), 0);
+    }
+    
+    @Test
+    public void testSizeOfIntersectionWith(){
+        Interval1D interval = new Interval1D(0,3);
+        Interval1D interval2 = new Interval1D(1,6);
+        assertEquals(3, interval.sizeOfIntersectionWith(interval2),0);
+    }
+    
+    @Test
+    public void testBoundingUnion(){
+        Interval1D interval = new Interval1D(0,3);
+        Interval1D interval2 = new Interval1D(6,7);
+        Interval1D interval3 = new Interval1D(0,7);
+        assertEquals(interval3, interval.boundingUnion(interval2));
+    }
+    
+    @Test
+    public void testIsUnionableWith(){
+        Interval1D interval = new Interval1D(0,3);
+        Interval1D interval2 = new Interval1D(2,7);
+        assertTrue(interval.isUnionableWith(interval2));
+    }
+    
+    @Test
+    public void testUnion(){
+        Interval1D interval = new Interval1D(0,3);
+        Interval1D interval2 = new Interval1D(2,5);
+        Interval1D interval3 = new Interval1D(0,5);
+        assertEquals(interval3, interval.union(interval2));
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testUnionException(){
+        Interval1D interval = new Interval1D(0,3);
+        Interval1D interval2 = new Interval1D(6,7);
+        interval.union(interval2);
+    }
+    
+    @Test
+    public void testEquals(){
+        Interval1D interval = new Interval1D(0,3);
+        Interval1D interval2 = new Interval1D(0,3);
+        assertTrue(interval.equals(interval2));
+    }
+    
+    @Test
+    public void testEqualsWrong(){
+        Interval1D interval = new Interval1D(0,3);
+        Interval1D interval2 = new Interval1D(5,8);
+        assertTrue(interval.equals(interval2));
+    }
+
+    @Test
+    public void testHashCode(){
+        
+    }
+    
+    @Test
+    public void testToString(){
+        Interval1D interval = new Interval1D(0,3);
+        System.out.println(interval.toString());
+        assertTrue(interval.toString().equals("[0..3]"));
+    }
+}
