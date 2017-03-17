5f535d5b8f7d7f8a8685065f1a2da3973e088ba0
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-03-17 18:05:06 +0100

    Enleve les warnings

--- test/ch/epfl/alpano/dem/HgtDiscreteElevationModelTest.java
+++ test/ch/epfl/alpano/dem/HgtDiscreteElevationModelTest.java
@@ -1,8 +1,5 @@
 package ch.epfl.alpano.dem;
 
-import static org.junit.Assert.*;
-import static ch.epfl.alpano.dem.DiscreteElevationModel.sampleIndex;
-import static java.lang.Math.toRadians;
 
 import java.io.File;
 
@@ -11,31 +8,35 @@ import org.junit.Test;
 public class HgtDiscreteElevationModelTest {
 
     @Test
-    public void testCornerSW() {
+    public void testCornerSW() throws Exception {
         HgtDiscreteElevationModel h = new HgtDiscreteElevationModel(new File("N46E007.hgt"));
         System.out.println(7.+" "+46.);
         h.elevationSample(7*3600, 3600*46); 
+        h.close();
     }
     
     @Test
-    public void testCornerNW() {
+    public void testCornerNW() throws Exception {
         HgtDiscreteElevationModel h = new HgtDiscreteElevationModel(new File("N46E007.hgt"));
         //System.out.println(7.+" "+47.);
-        h.elevationSample(7*3600, 3600*47); 
+        h.elevationSample(7*3600, 3600*47);
+        h.close();
     }
     
     @Test
-    public void testCornerSE() {
+    public void testCornerSE() throws Exception {
         HgtDiscreteElevationModel h = new HgtDiscreteElevationModel(new File("N46E007.hgt"));
         System.out.println(8.+" "+46.);
         h.elevationSample(8*3600, 3600*46); 
+        h.close();
     }
 
     @Test
-    public void testCornerNE() {
+    public void testCornerNE() throws Exception {
         HgtDiscreteElevationModel h = new HgtDiscreteElevationModel(new File("N46E007.hgt"));
         //System.out.println(8.+" "+47.);
         h.elevationSample(8*3600, 3600*47); 
+        h.close();
     }
     
 }
