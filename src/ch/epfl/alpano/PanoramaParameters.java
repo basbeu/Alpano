6965af7e35294879ff7ae9d146c5cec36561b459
phili <phili@128.179.194.212>
2017-03-20 12:47:41 +0100

    PanoramaParam 2

--- src/ch/epfl/alpano/PanoramaParameters.java
+++ src/ch/epfl/alpano/PanoramaParameters.java
@@ -4,6 +4,7 @@ import static ch.epfl.alpano.Azimuth.isCanonical;
 import static ch.epfl.alpano.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 import static ch.epfl.alpano.Math2.PI2;
+import static ch.epfl.alpano.Azimuth.canonicalize;
 
 
 final public class PanoramaParameters {
@@ -66,26 +67,43 @@ final public class PanoramaParameters {
     public double azimuthForX(double x){
         checkArgument(x < 0 && x > (WIDTH -1));
         double delta = HORIZONTALFIELDOFVIEW/(WIDTH-1);
-        return 
+        return  canonicalize((delta*x)- (WIDTH/2) + CENTERAZIMUTH); 
+        //return canonicalize(CENTERAZIMUTH + (x-(WIDTH/2))*delta);
     }
     
     public double xForAzimuth(double a){
+        checkArgument(a<CENTERAZIMUTH+HORIZONTALFIELDOFVIEW && a>CENTERAZIMUTH-HORIZONTALFIELDOFVIEW);
         
     }
     
     public double altitudeForY(double y){
-        
+        checkArgument(y<0 && y>(HEIGHT-1));
+        double delta = HORIZONTALFIELDOFVIEW/(WIDTH-1);
+        return canonicalize((delta*y)-(HEIGHT/2));
+        //return canonicalize(delta*(y-(HEIGHT/2)));
     }
     
     public double yForAltitude(double a){
+        checkArgument(a<verticalFieldOfView() && a>(PI2-verticalFieldOfView()));
         
+
     }
     
     boolean isValidSampleIndex(int x, int y){
-        
+        if(x > 0 && x < WIDTH && y > 0 && y < HEIGHT ){
+            return true;
+        }else
+            return false;
     }
     
     int linearSampleIndex(int x, int y){
+        if(y==0){
+            return x;
+        }else if(x==WIDTH-1 && y==HEIGHT-1){
+            return WIDTH*HEIGHT-1;
+        }else{
+            return (WIDTH-1)*y + x;
+        }
         
     }
 
