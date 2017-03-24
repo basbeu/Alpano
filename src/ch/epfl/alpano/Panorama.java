2b0f70a22a8bff9e7220d1da7143221a8e5c435d
phili <phili@192.168.0.150>
2017-03-24 15:02:43 +0100

    Builder

--- src/ch/epfl/alpano/Panorama.java
+++ src/ch/epfl/alpano/Panorama.java
@@ -1,5 +1,6 @@
 package ch.epfl.alpano;
 
+
 /**
  * Classe representant les panorama accompagnee d'un batisseur
  *
@@ -9,8 +10,83 @@ package ch.epfl.alpano;
 
 final class Panorama {
     
+    private final PanoramaParameters PARAMETERS;
+    private final float[] DISTANCE;
+    private final float[] LONGITUDE;
+    private final float[] LATITUDE;
+    private final float[] ELEVATION;
+    private final float[] SLOAP;
+
+    
     private Panorama (PanoramaParameters parameters, float[] distance, float[] longitude, float[] latitude,float[] elevation, float[] sloap){
+        PARAMETERS = parameters;    
+        DISTANCE = distance;
+        LONGITUDE = longitude;
+        LATITUDE = latitude;
+        ELEVATION = elevation;
+        SLOAP = sloap;
+    }
+    
+    public PanoramaParameters parameters(){
+        return PARAMETERS;
+    }
+    
+    public float distanceAt(int x, int y){
+        return DISTANCE[];
+    }
+    
+    public float longitudeAt(int x, int y){
+        return;
+    }
+    
+    public float latitudeAt(int x, int y){
+        return;
+    }
+    
+    public float elevationAt(int x, int y){
+        return;
+    }
+    
+    public float slopeAt(int x, int y){
+        return;
+    }
+    
+    public float distanceAt(int x, int y, float d){
+        return;
+    }
+    
+    public final static Builder {
+        public Builder(PanoramaParameters parameters){
+            
+        }
+        
+        public Builder setDistanceAt(int x, int y, float distance){
+            
+        }
+        
+        public Builder setLongitudeAt(int x, int y, float longitude){
+            
+        }
+        
+        public Builder setLatitudeAt(int x, int y, float latitude){
+            
+        }
+        
+        public Builder setElevationAt(int x, int y, float elevation){
+            
+        }
+        
+        public Builder setSlopeAt(int x, int y, float slope){
+            
+        }
+        
+        public Panorama build(){
+            
+        }
+        
         
     }
 
 }
+
+
