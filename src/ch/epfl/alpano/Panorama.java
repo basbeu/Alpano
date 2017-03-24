7f86f7de08e00ad174ad6c57ab051c6f9184f30f
phili <phili@192.168.0.150>
2017-03-24 13:25:32 +0100

    Creation class Panorama

---
+++ src/ch/epfl/alpano/Panorama.java
@@ -0,0 +1,16 @@
+package ch.epfl.alpano;
+
+/**
+ * Classe representant les panorama accompagnee d'un batisseur
+ *
+ * @author Philippine Favre (258854)
+ * @author Bastien Beuchat  (257117)
+ */
+
+final class Panorama {
+    
+    private Panorama (PanoramaParameters parameters, float[] distance, float[] longitude, float[] latitude,float[] elevation, float[] sloap){
+        
+    }
+
+}
