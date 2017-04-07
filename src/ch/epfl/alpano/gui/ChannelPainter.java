be0af3b64c4ab09e84fbaa07dd539220ac4a968d
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-04-07 16:27:40 +0200

    Déclaration méthode abstraite

--- src/ch/epfl/alpano/gui/ChannelPainter.java
+++ src/ch/epfl/alpano/gui/ChannelPainter.java
@@ -6,6 +6,8 @@ package ch.epfl.alpano.gui;
  * @author Philippine Favre (258854)
  * @author Bastien Beuchat  (257117)
  */
+@FunctionalInterface
 public interface ChannelPainter {
-
+    float valueAt(int x, int y);
+    
 }
