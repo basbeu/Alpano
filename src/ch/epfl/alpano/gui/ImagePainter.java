45838b48d9fc54087e48d49043a5c5c11ef82547
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-04-07 17:06:58 +0200

    ImagePainter et PanoramaRenderer

--- src/ch/epfl/alpano/gui/ImagePainter.java
+++ src/ch/epfl/alpano/gui/ImagePainter.java
@@ -10,13 +10,33 @@ import javafx.scene.paint.Color;
  */
 @FunctionalInterface
 public interface ImagePainter {
+    /**
+     * Donne la couleur de l'image pour un pixel donne
+     * @param x int coordonnee x du pixel
+     * @param y int coordonnee y du pixel
+     * @return Color (javafx.scene.paint.Color) representant la couleur du pixel
+     */
     Color colorAt(int x,int y);
     
+    /**
+     * Retourne un peintre HSB
+     * @param h ChannelPainter representant le canal de la teinte (hue)
+     * @param s ChannelPainter representant le canal de la saturation
+     * @param b ChannelPainter representant le canal de la luminosité
+     * @param o ChannelPainter representant le canal de l'opacité
+     * @return ImagePainter representant un peintre HSB
+     */
     static ImagePainter hsb(ChannelPainter h, ChannelPainter s, ChannelPainter b, ChannelPainter o){
-        return null;
+        return (x,y)->Color.hsb(h.valueAt(x,y), s.valueAt(x,y), b.valueAt(x,y), o.valueAt(x,y));
     }
     
+    /**
+     * Retourne un peintre en niveau de gris
+     * @param g ChannelPainter representant le canal de gris
+     * @param o ChannelPainter representant le canal d'opacité
+     * @return ImagePainter representant un peintre en niveau de gris
+     */
     static ImagePainter gray(ChannelPainter g, ChannelPainter o){
-        return null;
+        return (x,y)->Color.gray(g.valueAt(x,y),o.valueAt(x,y));
     }
 }
--- src/ch/epfl/alpano/gui/PanoramaRenderer.java
+++ src/ch/epfl/alpano/gui/PanoramaRenderer.java
@@ -1,11 +1,34 @@
 package ch.epfl.alpano.gui;
 
+import ch.epfl.alpano.Panorama;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+
 /**
- * 
+ * Interface permettant d'obtenir une image d'un panorama
  *
  * @author Philippine Favre (258854)
  * @author Bastien Beuchat  (257117)
  */
 public interface PanoramaRenderer {
-
+    
+    /**
+     * Crée l'image d'un panorama
+     * @param panorama Panorama representant le panorama a peindre
+     * @param painter ImagePainter representant la maniere de peindre le panorama
+     * @return Image representant l'image du panorama selon le peintre
+     */
+    static Image renderPanorama(Panorama panorama, ImagePainter painter){
+        WritableImage image = new WritableImage(panorama.parameters().width(),panorama.parameters().height());
+        PixelWriter writer = image.getPixelWriter();
+        
+        for(int x = 0;x < image.getWidth();++x){
+            for(int y = 0;y < image.getHeight();++y){
+                writer.setColor(x, y, painter.colorAt(x, y));
+            }
+        }
+        
+        return image;
+    }
 }
