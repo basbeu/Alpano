dc1f14d5b0af4d616609ff9ef1705ab412c6dcfe
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-04-07 15:05:57 +0200

    Interface public

--- src/ch/epfl/alpano/gui/ImagePainter.java
+++ src/ch/epfl/alpano/gui/ImagePainter.java
@@ -1,11 +1,22 @@
 package ch.epfl.alpano.gui;
 
+import javafx.scene.paint.Color;
+
 /**
- * 
+ * Interface fonctionnelle representant un peintre d'image
  *
  * @author Philippine Favre (258854)
  * @author Bastien Beuchat  (257117)
  */
+@FunctionalInterface
 public interface ImagePainter {
-
+    Color colorAt(int x,int y);
+    
+    static ImagePainter hsb(ChannelPainter h, ChannelPainter s, ChannelPainter b, ChannelPainter o){
+        return null;
+    }
+    
+    static ImagePainter gray(ChannelPainter g, ChannelPainter o){
+        return null;
+    }
 }
