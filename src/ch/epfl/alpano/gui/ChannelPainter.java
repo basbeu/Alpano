01b3f17331bdb3bdb2063c9517225b125d80a0f7
phili <phili@128.179.194.110>
2017-04-07 18:41:06 +0200

    ChanelPainter debut

--- src/ch/epfl/alpano/gui/ChannelPainter.java
+++ src/ch/epfl/alpano/gui/ChannelPainter.java
@@ -1,13 +1,54 @@
 package ch.epfl.alpano.gui;
 
+import java.util.function.DoubleUnaryOperator;
+
+import ch.epfl.alpano.Panorama;
+
 /**
- * 
+ * Interface fonctionnelle représentant un peintre de canal
  *
  * @author Philippine Favre (258854)
  * @author Bastien Beuchat  (257117)
  */
 @FunctionalInterface
 public interface ChannelPainter {
-    float valueAt(int x, int y);
+    abstract float valueAt(int x, int y);
+    
+    static ChannelPainter maxDistanceToNeighbors(Panorama panorama){
+        return null;
+        
+    }
+    
+    default void add(int c){
+        
+    }
+    
+    default void sub(int c){
+        
+    }
+    
+    default void mul(int c){
+        
+    }
+    
+    default void div(int c){
+        
+    }
+    
+    default void map(DoubleUnaryOperator op){
+        
+    }
+    
+    default void inverted(){
+        
+    }
+    
+    default void clamped(){
+        
+    }
+    
+    default void cycling(){
+        
+    }
     
 }
