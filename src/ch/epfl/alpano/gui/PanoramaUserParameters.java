8e2e3b979fdf39d30d2fe17b7e12d4abecb8c309
Bastien Beuchat <bastien.beuchat@epfl.ch>
2017-04-12 14:00:39 +0200

    Modifs syntaxe EnumMap

--- src/ch/epfl/alpano/gui/PanoramaUserParameters.java
+++ src/ch/epfl/alpano/gui/PanoramaUserParameters.java
@@ -19,123 +19,124 @@ import ch.epfl.alpano.PanoramaParameters;
  * @author Bastien Beuchat  (257117)
  */
 public final class PanoramaUserParameters {
-    
+
     private static final int TO_KM = 1_000;
     private static final int MAX_VERTICAL_FIELD_OF_VIEW = 170;
-    
+
     private Map<UserParameter, Integer> userParameters;
-    
+
     public PanoramaUserParameters(Map<UserParameter, Integer> userParameters){
         for(UserParameter param : UserParameter.values())
             checkArgument(userParameters.containsKey(param),"La Map doit contenir tous les paramêtres");
-            
+
         //controle de la hauteur
         int height = UserParameter.HEIGHT.sanitize(userParameters.get(UserParameter.HEIGHT));
         int width =  UserParameter.WIDTH.sanitize(userParameters.get(UserParameter.WIDTH));
         int vh = UserParameter.HORIZONTAL_FIELD_OF_VIEW.sanitize(userParameters.get(UserParameter.HORIZONTAL_FIELD_OF_VIEW));
-        
+
         if(((height-1)/(width-1))*vh > MAX_VERTICAL_FIELD_OF_VIEW)
             height = (MAX_VERTICAL_FIELD_OF_VIEW*(width-1))/vh;
-        
-        Map<UserParameter, Integer> up = new EnumMap<>(UserParameter.class);
-        up.put(UserParameter.OBSERVER_LONGITUDE, UserParameter.OBSERVER_LONGITUDE.sanitize(userParameters.get(UserParameter.OBSERVER_LONGITUDE)));
-        up.put(UserParameter.OBSERVER_LATITUDE, UserParameter.OBSERVER_LATITUDE.sanitize(userParameters.get(UserParameter.OBSERVER_LATITUDE)));
-        up.put(UserParameter.OBSERVER_ELEVATION, UserParameter.OBSERVER_ELEVATION.sanitize(userParameters.get(UserParameter.OBSERVER_ELEVATION)));
-        up.put(UserParameter.CENTER_AZIMUTH, UserParameter.CENTER_AZIMUTH.sanitize(userParameters.get(UserParameter.CENTER_AZIMUTH)));
-        up.put(UserParameter.HORIZONTAL_FIELD_OF_VIEW, vh);
-        up.put(UserParameter.MAX_DISTANCE, UserParameter.MAX_DISTANCE.sanitize(userParameters.get(UserParameter.MAX_DISTANCE)));
-        up.put(UserParameter.WIDTH, width);
-        up.put(UserParameter.HEIGHT, height);
-        up.put(UserParameter.SUPER_SAMPLING_EXPONENT, UserParameter.SUPER_SAMPLING_EXPONENT.sanitize(userParameters.get(UserParameter.SUPER_SAMPLING_EXPONENT)));
-                
+
+        Map<UserParameter, Integer> up = map(
+                UserParameter.OBSERVER_LONGITUDE.sanitize(userParameters.get(UserParameter.OBSERVER_LONGITUDE)),
+                UserParameter.OBSERVER_LATITUDE.sanitize(userParameters.get(UserParameter.OBSERVER_LATITUDE)),
+                UserParameter.OBSERVER_ELEVATION.sanitize(userParameters.get(UserParameter.OBSERVER_ELEVATION)),
+                UserParameter.CENTER_AZIMUTH.sanitize(userParameters.get(UserParameter.CENTER_AZIMUTH)),
+                vh,
+                UserParameter.MAX_DISTANCE.sanitize(userParameters.get(UserParameter.MAX_DISTANCE)),
+                width,
+                height,
+                UserParameter.SUPER_SAMPLING_EXPONENT.sanitize(userParameters.get(UserParameter.SUPER_SAMPLING_EXPONENT)));        
         this.userParameters = Collections.unmodifiableMap(new EnumMap<>(up));
     }
 
     public PanoramaUserParameters(int observerLongitude, int observerLatitude, int observerElevation, int centerAzimuth, int horizontalFieldOfView, int maxDistance, int width, int height, int superSamplingExponent){
-        
-        this(new HashMap<UserParameter, Integer>(){{
-            put(UserParameter.OBSERVER_LONGITUDE, observerLongitude);
-            put(UserParameter.OBSERVER_LATITUDE, observerLatitude);
-            put(UserParameter.OBSERVER_ELEVATION, observerElevation);
-            put(UserParameter.CENTER_AZIMUTH, centerAzimuth);
-            put(UserParameter.HORIZONTAL_FIELD_OF_VIEW, horizontalFieldOfView);
-            put(UserParameter.MAX_DISTANCE, maxDistance);
-            put(UserParameter.WIDTH, width);
-            put(UserParameter.HEIGHT, height);
-            put(UserParameter.SUPER_SAMPLING_EXPONENT, superSamplingExponent);
-        }});
-
+        this(map(observerLongitude, observerLatitude, observerElevation, centerAzimuth, horizontalFieldOfView, maxDistance, width, height, superSamplingExponent));
     }
-    
-    private double tenThousandthDegreesToRadian(int degree){
+
+    private static Map<UserParameter, Integer> map(int observerLongitude, int observerLatitude, int observerElevation, int centerAzimuth, int horizontalFieldOfView, int maxDistance, int width, int height, int superSamplingExponent){
+        Map<UserParameter, Integer> userParam = new EnumMap<>(UserParameter.class);
+        userParam.put(UserParameter.OBSERVER_LONGITUDE, observerLongitude);
+        userParam.put(UserParameter.OBSERVER_LATITUDE, observerLatitude);
+        userParam.put(UserParameter.OBSERVER_ELEVATION, observerElevation);
+        userParam.put(UserParameter.CENTER_AZIMUTH, centerAzimuth);
+        userParam.put(UserParameter.HORIZONTAL_FIELD_OF_VIEW, horizontalFieldOfView);
+        userParam.put(UserParameter.MAX_DISTANCE, maxDistance);
+        userParam.put(UserParameter.WIDTH, width);
+        userParam.put(UserParameter.HEIGHT, height);
+        userParam.put(UserParameter.SUPER_SAMPLING_EXPONENT, superSamplingExponent);
+        return userParam;
+    }
+
+    private static double tenThousandthDegreesToRadian(int degree){
         return toRadians(degree/10000);
     }
-    
+
     private GeoPoint observerPosition(){
         return new GeoPoint(tenThousandthDegreesToRadian(observerLongitude()), tenThousandthDegreesToRadian(observerLatitude())); 
     }
-    
+
     public int get(UserParameter up){
         return userParameters.get(up);
     }
-    
+
     public int observerLongitude(){
         return userParameters.get(UserParameter.OBSERVER_LONGITUDE);
     }
-    
+
     public int observerLatitude(){
         return userParameters.get(UserParameter.OBSERVER_LATITUDE);
     }
-    
+
     public int observerElevation(){
         return userParameters.get(UserParameter.OBSERVER_ELEVATION);
     }
-    
+
     public int centerAzimuth(){
         return userParameters.get(UserParameter.CENTER_AZIMUTH);
     }
-    
+
     public int horizontalFieldOfView(){
         return userParameters.get(UserParameter.HORIZONTAL_FIELD_OF_VIEW);
     }
-    
+
     public int maxDistance(){
         return userParameters.get(UserParameter.MAX_DISTANCE);
     }
-    
+
     public int width(){
         return userParameters.get(UserParameter.WIDTH);
     }
-    
+
     public int height(){
         return userParameters.get(UserParameter.HEIGHT);
     }
-    
+
     public int superSamplingExponent(){
         return userParameters.get(UserParameter.SUPER_SAMPLING_EXPONENT);
     }
-    
+
     public PanoramaParameters panoramaParameters(){
         int wp = (int)pow(2, superSamplingExponent())*width();
         int hp = (int)pow(2, superSamplingExponent())*height();
-        
+
         return new PanoramaParameters(observerPosition(), observerElevation(), toRadians(centerAzimuth()), toRadians(horizontalFieldOfView()), maxDistance() * TO_KM, wp, hp);
     }
-    
+
     public PanoramaParameters panoramaDisplayParameters(){
         return new PanoramaParameters(observerPosition(), observerElevation(), toRadians(centerAzimuth()), toRadians(horizontalFieldOfView()), maxDistance() * TO_KM, width(), height());
     }
-    
+
     @Override
     public boolean equals(Object t){
         return (t instanceof PanoramaUserParameters) && (((PanoramaUserParameters)t).userParameters.equals(userParameters));
     }
-    
+
     @Override
     public int hashCode(){
         return userParameters.hashCode();
     }
-    
-    
+
+
 
 }
