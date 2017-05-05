c27645bdc7b03d05217b2b27586db51374df258f
phili <phili@128.179.194.174>
2017-05-05 12:49:55 +0200

    Commentaires

--- src/ch/epfl/alpano/gui/FixedPointStringConverter.java
+++ src/ch/epfl/alpano/gui/FixedPointStringConverter.java
@@ -5,9 +5,21 @@ import java.math.RoundingMode;
 
 import javafx.util.StringConverter;
 
+/**
+ * Classe public, héritant de StringConverter<Integer>, convertit entre chaînes de caractères et entiers. 
+ *
+ * @author Philippine Favre (258854)
+ * @author Bastien Beuchat  (257117)
+ */
+
 public final class FixedPointStringConverter extends StringConverter<Integer>{
     
     private final int decimal;
+    
+    /**
+     * Constructeur d'un FixedPointStringConverter
+     * @param int arrondi à un nombre de décimale fixe 
+     */
     
     public FixedPointStringConverter(int decimal){
         this.decimal = decimal;
